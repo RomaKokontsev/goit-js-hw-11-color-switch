ab9ea6db32a06cafa6a523ced28c38aac1a6935c
Roma Kokontsev <Romeo42@ukr.net>
2021-05-25 13:49:49 +0300

    добавляет начальные файлы

---
+++ index.js
@@ -0,0 +1,14 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    buttonStart: document.querySelector('[data-action="start"]'),
+    buttonStop: document.querySelector('[data-action="stop"]'),
+    body : document.body
+}
